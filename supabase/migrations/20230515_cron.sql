ca355c32a6d012cc7c84a31107cd4da663737252
Saturnych <saturnych@gmail.com>
2023-05-19 19:18:03 +0300

    Create 20230515_cron.sql

---
+++ supabase/migrations/20230515_cron.sql
@@ -0,0 +1,13 @@
+select cron.unschedule('cron-function-every-day');
+select cron.schedule(
+    'cron-function-every-day',
+    '0 0 * * *', -- every day
+    $$
+    select
+      net.http_post(
+          url:='https://cxopfnqvgblflivwrubv.functions.supabase.co/tgwebapp-cron?secret=cxopfnqvgblflivwrubv&name=cron-function-every-day',
+          headers:='{"Content-Type": "application/json", "Authorization": "Bearer cxopfnqvgblflivwrubv"}'::jsonb,
+          body:=concat('{"time": "', now(), '"}')::jsonb
+      ) as request_id;
+    $$
+);
