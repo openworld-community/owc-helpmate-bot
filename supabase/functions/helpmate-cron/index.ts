a68af3bc157e0bb8654676c59d5bdb0eacc59a1b
Saturnych <saturnych@gmail.com>
2023-05-19 19:17:54 +0300

    Create index.ts

---
+++ supabase/functions/helpmate-cron/index.ts
@@ -0,0 +1,45 @@
+import { serve } from 'http/server';
+
+import ENV from '$lib/vars.ts';
+const { DEBUG, APP_NAME, SMTP_NOTIFY, CRON_SECRET, ADMIN_IDS, TELEGRAM_BOT_SEND_MESSAGE } = ENV;
+
+console.info(`Cron "${APP_NAME}" up and running!`);
+
+const sendMessage = async (message, contentType = 'application/json', method = 'post') => {
+  if (!!!TELEGRAM_BOT_SEND_MESSAGE) return `{"ok": false, "error": "add 'TELEGRAM_BOT_SEND_MESSAGE' secret!"}`;
+
+  return await (await fetch(TELEGRAM_BOT_SEND_MESSAGE, {
+    method,
+    headers: {
+      'Accept': contentType,
+      'Content-Type': contentType,
+    },
+    body: JSON.stringify(message),
+  })).json();
+};
+
+try {
+  if (!!CRON_SECRET) {
+    serve(async (req) => {
+      try {
+        const url = new URL(req.url);
+        if (url.searchParams.get('secret') !== CRON_SECRET) {
+          return new Response('405 Not allowed', { status: 405 });
+        }
+        const name = url.searchParams.get('name');
+        const text = `Cronjob '${name}' is done at ${new Date}`;
+        const results = [];
+        for (let i=0;i<ADMIN_IDS.length;i++) {
+          results.push(await sendMessage({ chat_id: ADMIN_IDS[i], text }));
+        }
+        //const sent = await smtpSendOnce('Test email from supabase edge function', '<h1>Header</h1><p>Hi!</p>', SMTP_NOTIFY);
+        return new Response(JSON.stringify(results,null,2), { status: 200, headers: { 'Content-Type': 'application/json' }})
+      } catch (err) {
+        console.error(err);
+        return new Response(String(err?.message ?? err), { status: 500 });
+      }
+    });
+  }
+} catch (err) {
+  console.error(err);
+}
