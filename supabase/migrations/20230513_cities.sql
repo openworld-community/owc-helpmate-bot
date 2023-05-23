d304edb2cf98e84f5ee4c8fcf75741c5023e59eb
Saturnych <saturnych@gmail.com>
2023-05-23 17:56:11 +0300

    Update 20230513_cities.sql

--- supabase/migrations/20230513_cities.sql
+++ supabase/migrations/20230513_cities.sql
@@ -1,14 +1,13 @@
-CREATE TABLE public.cities (
-  id BIGINT NOT NULL,
-  country BIGINT NULL,
-  state BIGINT NULL,
-  name VARCHAR NULL DEFAULT ''::character varying ,
-  latitude NUMERIC NULL,
-  longitude NUMERIC NULL,
-  created_at TIMESTAMP WITH TIME ZONE NULL DEFAULT now() ,
-  updated_at TIMESTAMP WITH TIME ZONE NULL DEFAULT now() ,
-  CONSTRAINT cities_pkey PRIMARY KEY (id),
-  CONSTRAINT cities_country_fkey FOREIGN KEY (country) REFERENCES public.countries (id) ON DELETE CASCADE ON UPDATE NO ACTION,
-  CONSTRAINT cities_state_fkey FOREIGN KEY (state) REFERENCES public.states (id) ON DELETE CASCADE ON UPDATE NO ACTION,
-  CONSTRAINT cities_un_id UNIQUE (id)
+CREATE TABLE "public"."cities" (
+  "id" BIGINT NOT NULL,
+  "country" BIGINT NULL,
+  "state" BIGINT NULL,
+  "name" VARCHAR NULL DEFAULT ''::character varying ,
+  "latitude" NUMERIC NULL,
+  "longitude" NUMERIC NULL,
+  "created_at" TIMESTAMP WITH TIME ZONE NULL DEFAULT now() ,
+  "updated_at" TIMESTAMP WITH TIME ZONE NULL DEFAULT now() ,
+  CONSTRAINT "cities_pkey" PRIMARY KEY ("id")
 );
+ALTER TABLE "public"."cities" ADD CONSTRAINT "cities_country_fkey" FOREIGN KEY ("country") REFERENCES "public"."countries" ("id") ON DELETE CASCADE ON UPDATE NO ACTION;
+ALTER TABLE "public"."cities" ADD CONSTRAINT "cities_state_fkey" FOREIGN KEY ("state") REFERENCES "public"."states" ("id") ON DELETE CASCADE ON UPDATE NO ACTION;
