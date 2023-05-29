e9f54943cf3b57c365484de8aa3ce9a0440ef8c4
Saturnych <saturnych@gmail.com>
2023-05-29 16:21:01 +0300

    Update 20230519_tasks.sql

--- supabase/migrations/20230519_tasks.sql
+++ supabase/migrations/20230519_tasks.sql
@@ -4,14 +4,15 @@ DROP TABLE IF EXISTS public.tasks;
 create table public.tasks (
   uid uuid NOT NULL DEFAULT gen_random_uuid(),
   chat BIGINT NULL,
-  user BIGINT NULL,
+  profile BIGINT NULL,
   helper BIGINT NULL,
-  status VARCHAR NULL DEFAULT 'open'::character varying , -- open bad closed
+  status VARCHAR NULL DEFAULT 'open'::character varying , -- open bad closed expired
   description TEXT NULL,
   expiry_date TIMESTAMP WITH TIME ZONE NULL DEFAULT now() ,
   created_at TIMESTAMP WITH TIME ZONE NULL DEFAULT now() ,
   updated_at TIMESTAMP WITH TIME ZONE NULL DEFAULT now() ,
   CONSTRAINT tasks_pkey PRIMARY KEY (uid),
   CONSTRAINT tasks_chat_fkey FOREIGN KEY (chat) REFERENCES public.chats (id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT tasks_profile_fkey FOREIGN KEY (profile) REFERENCES public.profiles (id) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT tasks_helper_fkey FOREIGN KEY (helper) REFERENCES public.helpers (id) ON DELETE NO ACTION ON UPDATE NO ACTION
 );
